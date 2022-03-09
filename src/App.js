6c04e1c40e283ffc42bef59e159e0fb5b85f147f
bea andrea <petermagalong0421@gmail.com>
2022-03-09 01:10:48 -0800

    first commit fe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import HomeScreen from "./Screen/HomeScreen";
+import ProductScreen from "./Screen/ProductScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to ="/">Amazona</Link>
+        </header>
+        <main>
+        <Routes>
+          <Route path="/product/:slug" element={<ProductScreen />}></Route>
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
+      </main>
     </div>
+    </BrowserRouter>
   );
 }
 
